a30a0aa49dc338689fc4523d729d009267da1f4a
Alex Leventer <alexleventer@gmail.com>
2020-07-22 17:02:19 -0700

    Clean up config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME || 'test123';
-export const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD || 'test123';
-export const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE || 'test';
+export const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME;
+export const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD;
+export const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE;
 export const ENTER_KEY = 13;
 export const API_ENDPOINT = `/api/rest/v1`;
