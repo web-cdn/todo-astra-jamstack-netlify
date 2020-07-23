05a47e5d65432085a549ac3e9e16d3f9c0c2660d
Alex Leventer <alexleventer@gmail.com>
2020-07-22 20:43:14 -0700

    Update setup script

--- gitpod-setup.sh
+++ gitpod-setup.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
-echo "What is your Astra DB username?"
+echo "What is your Astra DB username? 🚀"
 read ASTRA_DB_USERNAME
-echo "\nWhat is your Astra DB password?"
-read ASTRA_DB_PASSWORD
-echo "\nWhat is your Astra keyspace name?"
+echo "\nWhat is your Astra DB password? 🔒"
+read -s ASTRA_DB_PASSWORD
+echo "\nWhat is your Astra keyspace name? 🔑"
 read ASTRA_DB_KEYSPACE
-echo "\nWhat is your Astra endpoint? (Example: https://4e62bc79-0e12-us-east1.apps.astra.datastax.com)"
-read ASTRA_ENDPOINT
-eval $(gp env -e ASTRA_DB_USERNAME=$ASTRA_DB_USERNAME ASTRA_DB_PASSWORD=$ASTRA_DB_PASSWORD ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE ASTRA_ENDPOINT=$ASTRA_ENDPOINT)
+echo "\nWhat is your Astra database id? Example: 4e62bc79-0e12-4667-bd7d-2191ece2a32c ☁️"
+read ASTRA_DB_ID
+
+echo "\nWhat is your Astra database region? Example: us-east1 🌍"
+read ASTRA_DB_REGION
+
+# Set GitPod.io environment variables
+eval $(gp env -e ASTRA_DB_USERNAME=$ASTRA_DB_USERNAME ASTRA_DB_PASSWORD=$ASTRA_DB_PASSWORD ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE ASTRA_ENDPOINT=https://$ASTRA_DB_ID-$ASTRA_DB_REGIONapps.astra.datastax.com)
+
+# Get Astra auth token
+ENDPOINT="https://${ASTRA_DB_ID}-${ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1/auth"
+AUTH_TOKEN=$(curl --request POST \
+  --url $ENDPOINT \
+  --header 'content-type: application/json' \
+  --data '{"username":"'${ASTRA_DB_USERNAME}'","password":"'${ASTRA_DB_USERNAME}'"}'  | python -c "import sys, json; print json.load(sys.stdin)['authToken']")
+
+# Create todos table
+curl --request POST \
+  --url "https://${ASTRA_DB_ID}-${ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1/keyspaces/${ASTRA_DB_KEYSPACE}/tables" \
+  --header 'content-type: application/json' \
+  --header "x-cassandra-token: ${AUTH_TOKEN}" \
+  --data '{"ifNotExists":true,"columnDefinitions":[{"static":false,"name":"list_id","typeDefinition":"text"},{"static":false,"name":"id","typeDefinition":"timeuuid"},{"static":false,"name":"title","typeDefinition":"text"},{"static":false,"name":"completed","typeDefinition":"boolean"}],"primaryKey":{"partitionKey":["list_id","id"]},"tableOptions":{"defaultTimeToLive":0,"clusteringExpression":[{"column":"id","order":"DESC"}]},"name":"todos"}'
