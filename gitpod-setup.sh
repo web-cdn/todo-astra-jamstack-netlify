eeb06f4d92e51040cca990a614d4bd313de0085e
Alex Leventer <alexleventer@gmail.com>
2020-07-22 19:59:45 -0700

    Improve shell script

--- gitpod-setup.sh
+++ gitpod-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo What is your Astra DB username?
+echo "What is your Astra DB username?"
 read ASTRA_DB_USERNAME
-echo What is your Astra DB password?
+echo "\nWhat is your Astra DB password?"
 read ASTRA_DB_PASSWORD
-echo What is your Astra keyspace name?
+echo "\nWhat is your Astra keyspace name?"
 read ASTRA_DB_KEYSPACE
-echo What is your Astra endpoint?
+echo "\nWhat is your Astra endpoint? (Example: https://4e62bc79-0e12-us-east1.apps.astra.datastax.com)"
 read ASTRA_ENDPOINT
 eval $(gp env -e ASTRA_DB_USERNAME=$ASTRA_DB_USERNAME ASTRA_DB_PASSWORD=$ASTRA_DB_PASSWORD ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE ASTRA_ENDPOINT=$ASTRA_ENDPOINT)
