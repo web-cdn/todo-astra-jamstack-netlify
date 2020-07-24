6294db538b50c0a87f1e2abbeb2478f8c7b8df2c
Alex Leventer <alexleventer@gmail.com>
2020-07-24 10:44:06 -0700

    Only ask for env variables that are not set

--- gitpod-setup.sh
+++ gitpod-setup.sh
@@ -1,23 +1,36 @@
 #!/bin/sh
-echo "What is your Astra DB username? 🚀"
-read ASTRA_DB_USERNAME
-echo "\nWhat is your Astra DB password? 🔒"
-read ASTRA_DB_PASSWORD
-echo "\nWhat is your Astra keyspace name? 🔑"
-read ASTRA_DB_KEYSPACE
-echo "\nWhat is your Astra database id? Example: 4e62bc79-0e12-4667-bd7d-2191ece2a32c ☁️"
-read ASTRA_DB_ID
+if [[ -z "${ASTRA_DB_USERNAME}" ]]; then
+  echo "What is your Astra DB username? 🚀"
+  read -r ASTRA_DB_USERNAME
+fi
 
-echo "\nWhat is your Astra database region? Example: us-east1 🌍"
-read ASTRA_DB_REGION
+if [[ -z "${ASTRA_DB_PASSWORD}" ]]; then
+  echo "\nWhat is your Astra DB password? 🔒"
+  read -r ASTRA_DB_PASSWORD
+fi
+
+if [[ -z "${ASTRA_DB_KEYSPACE}" ]]; then
+  echo "\nWhat is your Astra keyspace name? 🔑"
+  read -r ASTRA_DB_KEYSPACE
+fi
+
+if [[ -z "${ASTRA_DB_ID}" ]]; then
+  echo "\nWhat is your Astra database id? Example: 4e62bc79-0e12-4667-bd7d-2191ece2a32c ☁️"
+  read -r ASTRA_DB_ID
+fi
+
+if [[ -z "${ASTRA_DB_REGION}" ]]; then
+  echo "\nWhat is your Astra database region? Example: us-east1 🌍"
+  read -r ASTRA_DB_REGION
+fi
 
 ASTRA_ENDPOINT="https://${ASTRA_DB_ID}-${ASTRA_DB_REGION}.apps.astra.datastax.com"
 
 # Set GitPod.io environment variables
 eval $(gp env -e ASTRA_DB_USERNAME=$ASTRA_DB_USERNAME ASTRA_DB_PASSWORD=$ASTRA_DB_PASSWORD ASTRA_DB_KEYSPACE=$ASTRA_DB_KEYSPACE ASTRA_ENDPOINT=$ASTRA_ENDPOINT)
-eval $(gp env -e)
 
 # Get Astra auth token
+echo "Getting your Astra auth token..."
 ENDPOINT="https://${ASTRA_DB_ID}-${ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1/auth"
 AUTH_TOKEN=$(curl --request POST \
   --url $ENDPOINT \
@@ -25,6 +38,7 @@ AUTH_TOKEN=$(curl --request POST \
   --data '{"username":"'${ASTRA_DB_USERNAME}'","password":"'${ASTRA_DB_USERNAME}'"}'  | jq -r '.authToken')
 
 # Create todos table
+echo "Creating Astra tables..."
 curl --request POST \
   --url "https://${ASTRA_DB_ID}-${ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1/keyspaces/${ASTRA_DB_KEYSPACE}/tables" \
   --header 'content-type: application/json' \
