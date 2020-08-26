e5d3d48a8e4e940e03d621485eaab014b80c9b3f
Alex Leventer <alexleventer@gmail.com>
2020-08-26 14:35:56 -0700

    add error message if auth fails

--- functions/utils/getAuthToken.js
+++ functions/utils/getAuthToken.js
@@ -1,7 +1,7 @@
 const fetch = require('cross-fetch')
-const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`
-const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME
-const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD
+const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`;
+const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME;
+const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD;
 
 module.exports = async function getAuthToken() {
   try {
@@ -14,8 +14,8 @@ module.exports = async function getAuthToken() {
         username: ASTRA_DB_USERNAME,
         password: ASTRA_DB_PASSWORD,
       }),
-    }).then(res => res.json())
+    }).then(res => res.json());
   } catch (e) {
-    // TODO: Do something here
+    throw new Error('Could not authenticate with the Astra API');
   }
 }
