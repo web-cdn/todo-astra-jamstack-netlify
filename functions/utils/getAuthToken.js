fe39b9b04e29de1e1393aef942023574e460f905
Alex Leventer <alexleventer@gmail.com>
2020-08-25 18:31:58 -0700

    add netlify functions

---
+++ functions/utils/getAuthToken.js
@@ -0,0 +1,17 @@
+const fetch = require('cross-fetch')
+const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`
+const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME
+const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD
+
+module.exports = async function getAuthToken() {
+  return await fetch(`${ENDPOINT}/auth`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: ASTRA_DB_USERNAME,
+      password: ASTRA_DB_PASSWORD,
+    }),
+  }).then(res => res.json())
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,45 +1,19 @@
-import fetch from 'cross-fetch'
 import utils from './utils'
-const ASTRA_DB_USERNAME = process.env.ASTRA_DB_USERNAME
-const ASTRA_DB_PASSWORD = process.env.ASTRA_DB_PASSWORD
-const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE
-const API_ENDPOINT = `/api/rest/v1`
-const TABLE_NAME = process.env.TABLE_NAME || 'jamstack_todos'
-
-export const getAuthToken = async () => {
-  return await fetch(`${API_ENDPOINT}/auth`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      username: ASTRA_DB_USERNAME,
-      password: ASTRA_DB_PASSWORD,
-    }),
-  }).then(res => res.json())
-}
 
 export const deleteTodo = async (todo) => {
-  const { authToken } = await getAuthToken()
-  return await fetch(`${API_ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows/${todo.list_id};${todo.id}`, {
-    method: 'DELETE',
-    headers: {
-      'x-cassandra-token': authToken,
-    }
+  const response = await fetch('/.netlify/functions/deleteTodos', {
+    body: JSON.stringify({todo}),
+    method: 'POST'
   })
+  return response.json()
 }
 
 export const getTodos = async (sessionId) => {
-  const { authToken } = await getAuthToken()
-  return await fetch(`${API_ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows/${sessionId}`, {
-    headers: {
-      'x-cassandra-token': authToken,
-    }
-  }).then(res => res.json())
+  const response = await fetch(`/.netlify/functions/getTodos/${sessionId}`)
+  return response.json()
 }
 
 export const createTodo = async (todo, sessionId) => {
-  const { authToken } = await getAuthToken()
   if (!todo.id) {
     todo.id = utils.uuid()
   }
@@ -53,11 +27,9 @@ export const createTodo = async (todo, sessionId) => {
       }
     })
   }
-  return await fetch(`${API_ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows`, {
-    method: 'POST',
-    headers: {
-      'x-cassandra-token': authToken
-    },
-    body: JSON.stringify(columns)
+  const response = await fetch('/.netlify/functions/createTodo', {
+    body: JSON.stringify(columns),
+    method: 'POST'
   })
+  return response.json()
 }
