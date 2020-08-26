beba51ee6e816657af9b09e059cbf2951ea8e8fb
Alex Leventer <alexleventer@gmail.com>
2020-08-25 19:03:38 -0700

    remove env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-require('dotenv').config();
 
 const entry = './src/entry.jsx';
 const outputPath = path.resolve('./dist');
@@ -48,26 +46,16 @@ const clientConfig = {
   resolve,
   plugins: [
     // Copy all used resources (no dir available)
-    new Dotenv(),
     new CopyPlugin({
       patterns: [
         { from: "assets", to: "assets" },
         { from: "css", to: "css" },
         { from: "public" },
         { from: "index.html", to: "index.html", transform: (content) => {
-            if (process.env.PUBLIC_PATH) {
-              let path = process.env.PUBLIC_PATH;
-              if (path.endsWith("/")) {
-                path = path.substring(0, path.length - 1);
-              }
-              return String(content).replace(/{{CDN}}/g, path);
-            } else {
-              // Don`t use CDN
-              return String(content).replace(/{{CDN}}\//g, "");
-            }
-          }
+          return String(content).replace(/{{CDN}}\//g, "");
         }
-      ]}),
+      }
+    ]}),
   ]
 };
 
