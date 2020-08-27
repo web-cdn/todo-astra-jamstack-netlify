38241b9f443142e02538d28aff91c98f5785184b
phact <estevezsebastian@gmail.com>
2020-08-26 23:26:36 -0400

    test

--- functions/createTodo.js
+++ functions/createTodo.js
@@ -3,17 +3,21 @@ const getAuthToken = require('./utils/getAuthToken');
 const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`;
 const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE;
 const TABLE_NAME = 'jamstack_todos';
+let cachedToken
 
 exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
-  const {authToken} = await getAuthToken();
+  if(!cachedToken){
+    cachedToken = await getAuthToken();
+  }
+
 
   try {
     const response = await fetch(`${ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-cassandra-token': authToken,
+        'x-cassandra-token': cachedToken.authToken,
       },
       body: JSON.stringify({
         columns: body.columns
