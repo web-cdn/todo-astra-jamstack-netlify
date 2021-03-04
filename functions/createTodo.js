f3561f9393229ef2031311d9e8abe0bd538ab72d
CRW <chris.wilhite@datastax.com>
2021-03-04 11:09:54 -0500

    update for IAM

--- functions/createTodo.js
+++ functions/createTodo.js
@@ -1,19 +1,17 @@
 const fetch = require('cross-fetch');
-const getOrCreateAuthToken = require('./utils/getOrCreateAuthToken');
 const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`;
 const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE;
 const TABLE_NAME = 'jamstack_todos';
 
 exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
-  const {authToken} = await getOrCreateAuthToken();
 
   try {
     const response = await fetch(`${ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-cassandra-token': authToken,
+        'x-cassandra-token': process.env.ASTRA_DB_APPLICATION_TOKEN,
       },
       body: JSON.stringify({
         columns: body.columns
--- functions/getTodos.js
+++ functions/getTodos.js
@@ -1,5 +1,4 @@
 const fetch = require('cross-fetch');
-const getOrCreateAuthToken = require('./utils/getOrCreateAuthToken');
 const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`;
 const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE;
 const TABLE_NAME = 'jamstack_todos';
@@ -7,12 +6,11 @@ const TABLE_NAME = 'jamstack_todos';
 exports.handler = async (event, context) => {
   const {path} = event;
   const sessionId = path.split('getTodos/')[1];
-  const {authToken} = await getOrCreateAuthToken();
 
   try {
     const response = await fetch(`${ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows/${sessionId}`, {
       headers: {
-        'x-cassandra-token': authToken
+        'x-cassandra-token': process.env.ASTRA_DB_APPLICATION_TOKEN,
       }
     });
     return {
